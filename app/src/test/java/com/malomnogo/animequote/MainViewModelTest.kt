2e69ff8bca73ce8b00366eb0218db84a2a635cd4
Eugene <48792577+Malomnogo@users.noreply.github.com>
2024-02-11 20:48:02 +0300

    AQ-08 MainViewModelTest (#9)

---
+++ app/src/test/java/com/malomnogo/animequote/MainViewModelTest.kt
@@ -0,0 +1,130 @@
+package com.malomnogo.animequote
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class MainViewModelTest {
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var repository: FakeRepository
+    private lateinit var observable: FakeUiObservable
+    private lateinit var runAsync: FakeRunAsync
+
+    @Before
+    fun setup() {
+        repository = FakeRepository()
+        observable = FakeUiObservable()
+        runAsync = FakeRunAsync()
+        viewModel = MainViewModel(
+            repository = repository,
+            observable = observable,
+            runAsync = runAsync
+        )
+    }
+
+    @Test
+    fun testFirstOpen() {
+        viewModel.init(isFirtsOpen = true)
+        assertEquals(listOf(QuoteUiState.Loading), observable.states)
+
+        val uiCallback = object : UiCallBack {
+            override fun updateUi(uiState: QuoteUiState) = Unit
+        }
+
+        viewModel.startGettingUpdates(uiCallback)
+        assertEquals(listOf(uiCallback), observable.observers)
+
+        viewModel.stopGettingUpdates()
+        assertEquals(listOf(uiCallback, QuoteUiState.Empty), observable.observers)
+    }
+
+    @Test
+    fun testNotFirstRun() {
+        viewModel.init(isFirstOpen = false)
+        assertEquals(emptyList<QuoteUiState>(), observable.states)
+        assertEquals(QuoteUiState.Empty, observable.observers)
+    }
+
+    @Test
+    fun testSuccess() {
+        repository.result = LoadResult.Success(quote = "Fake quote text")
+        viewModel.load()
+        assertEquals(listOf(QuoteUiState.Progress), observable.states)
+
+        runAsync.returnResult()
+        assertEquals(
+            listOf(
+                QuoteUiState.Progress,
+                QuoteUiState.Success(quote = "Fake quote text")
+            ), observable.states
+        )
+    }
+
+    @Test
+    fun testError() {
+        repository.result = LoadResult.Error(message = "No internet connection")
+        viewModel.load()
+        assertEquals(listOf(QuoteUiState.Progress), observable.states)
+
+        runAsync.returnResult()
+        assertEquals(
+            listOf(
+                QuoteUiState.Progress,
+                QuoteUiState.Error(message = "Fake quote text")
+            ), observable.states
+        )
+    }
+
+}
+
+private class FakeRepository : Repository {
+
+    lateinit var result: LoadResult
+    private var cachedQuote = ""
+
+    override suspend fun loadData() = result
+
+    override suspend fun saveJoke(text: String) {
+        cachedQuote = text
+    }
+
+}
+
+class FakeUiObservable : UiObservable {
+
+    val states = mutableListOf<QuoteUiState>()
+    val observers = mutableListOf<QuoteUiState>()
+
+    override fun updateUi(uiState: QuoteUiState) {
+        states.add(uiState)
+    }
+
+    override fun updateObserver(observer: UiCallBack) {
+        observers.add(observer)
+        updateUi(states.last())
+    }
+}
+
+class FakeRunAsync : RunAsync {
+
+    private var cachedUiBlock: (Any) -> Unit = {}
+    private var cachedResult: Any = ""
+
+    fun returnResult() {
+        cachedUiBlock.invoke(cachedResult)
+    }
+
+    override fun <T : Any> start(
+        coroutineScope: CoroutineScope,
+        background: suspend () -> T,
+        uiBlock: (T) -> Unit,
+    ) = runBlocking {
+        val result = background.invoke()
+        cachedResult = result
+        cachedUiBlock = uiBlock as (Any) -> Unit
+    }
+
+}
