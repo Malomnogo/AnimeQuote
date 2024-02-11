ded962a9bf3e4bd946b6c9ed599ff996c5bd7ff3
Eugene <48792577+Malomnogo@users.noreply.github.com>
2024-02-11 17:50:53 +0300

    fix fragment logic to one activity without navigation (#7)

--- app/src/androidTest/java/com/malomnogo/animequote/AbstractPage.kt
+++ app/src/androidTest/java/com/malomnogo/animequote/AbstractPage.kt
@@ -1,16 +1,36 @@
 package com.malomnogo.animequote
 
 import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.ProgressBar
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import org.hamcrest.CoreMatchers.allOf
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.*
 
 abstract class AbstractPage {
 
-    abstract fun checkVisible()
+    open fun checkVisible() {
+        onView(
+            allOf(
+                withId(R.id.progressBar),
+                isAssignableFrom(ProgressBar::class.java)
+            )
+        ).check(matches(not(isDisplayed())))
+
+        onView(
+            allOf(
+                withId(R.id.nextButton),
+                isAssignableFrom(Button::class.java),
+                withParent(withId(R.id.rootLayout)),
+                withParent(isAssignableFrom(FrameLayout::class.java))
+            )
+        ).check(matches(isDisplayed()))
+    }
 
     open fun clickNext() {
         onView(
