968ecf201321b78207c19383d0ae14f5804496f5
Eugene <auricamercer@gmail.com>
2024-02-11 22:00:25 +0300

    AQ-14 UiObservable

---
+++ app/src/main/java/com/malomnogo/animequote/UiObservable.kt
@@ -0,0 +1,25 @@
+package com.malomnogo.animequote
+
+interface UiObservable : UpdateUi, UpdateObserver {
+
+
+    class Base : UiObservable {
+
+        private var cache: QuoteUiState = QuoteUiState.Empty
+        private var observer: UpdateUi = UpdateUi.Empty
+
+        override fun update(uiState: QuoteUiState) {
+            cache = uiState
+            observer.update(cache)
+        }
+
+        override fun updateObserver(observer: UpdateUi) {
+            this.observer = observer
+            observer.update(cache)
+        }
+    }
+}
+
+interface UpdateObserver {
+    fun updateObserver(observer: UpdateUi)
+}
