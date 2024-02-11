1c5e54ce69f143d4aa5c163183b91aaf9cd5583a
Eugene <auricamercer@gmail.com>
2024-02-11 22:01:49 +0300

    AQ-15 LoadResult

---
+++ app/src/main/java/com/malomnogo/animequote/LoadResult.kt
@@ -0,0 +1,17 @@
+package com.malomnogo.animequote
+
+
+interface LoadResult {
+
+    fun handle(observable: UiObservable)
+
+    abstract class Base(private val updateState: QuoteUiState) : LoadResult {
+        override fun handle(observable: UiObservable) {
+            observable.update(updateState)
+        }
+    }
+
+    data class Success(private val quote: String) : Base(QuoteUiState.Success(quote))
+
+    data class Error(private val message: String) : Base(QuoteUiState.Error(message))
+}
